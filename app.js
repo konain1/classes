62f25b6b8760b53df7f3d7430eb363195aa7f890
Konain1 <kaunainkasab@gmail.com>
2023-11-02 15:52:16 +0530

    super and extends key understood

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ class Pet {
     return `${this.name} is eating`
    }
 }
+// super is reference of extends class  ex : Pet class
+
 
 class Cat extends Pet{
-
+    constructor(name,age,liveleft=9){
+        super(name,age)
+        this.liveleft = liveleft
+    }
     meow(){
         return `MEOW!!!`
+    }
+    details(){
+        return `${this.name} has liveleft ${this.liveleft}`
     }
 }
 class Dog extends Pet {
@@ -24,6 +32,6 @@ class Dog extends Pet {
 
 const doora = new Cat('doora',3)
 const tiger = new Dog('tiger',12)
-console.log(doora.eat())
+console.log(doora.details())
 console.log(tiger.eat())
 
